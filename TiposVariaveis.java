f64abcdb9225e18e68a66bae2aff49ef8ed9714d
izequiel <izequiel.r.alves@gmail.com>
2026-07-23 23:16:33 -0300

    Conceitos sobre tipos e variaveis.

--- TiposVariaveis.java
+++ TiposVariaveis.java
@@ -1,4 +1,20 @@
 package estudos.Izequiel.sintaxejava.trilhaDIO;
 
 public class TiposVariaveis {
+
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+
+        short nomeroCurto = 1;
+        int numeroNormal = nomeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        //dessa forma aqui é conhecido como variavel, pois pode ser alterado.
+
+
+        final double valorPI = 3.14;
+        //final representa algo que n pode ser alterado, mais conhecido como "constante"
+    }
 }
